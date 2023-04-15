fb863579f3e35a39d4af23b9fc21c63b3be9ca82
suraj_gotad <surajgotad111@gmail.com>
2023-04-15 13:46:53 +0530

    Hashset Added

---
+++ src/com/velocity/demo/HashsetDemo.java
@@ -0,0 +1,15 @@
+package com.velocity.demo;
+
+import java.util.HashSet;
+
+public class HashsetDemo {
+	public static void main(String[] args) {
+		HashSet<Integer> hash=new HashSet<Integer>();
+		hash.add(100);
+		hash.add(200);
+		hash.add(300);
+		
+		System.out.println(hash);
+	}
+
+}
