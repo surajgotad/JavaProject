a8bcd7a6f0664065a805ea0116a45957b7c72b8b
suraj_gotad <surajgotad111@gmail.com>
2023-04-15 14:25:45 +0530

    Jump Updated

--- src/com/velocity/demo/jump.java
+++ src/com/velocity/demo/jump.java
@@ -1,3 +1,9 @@
-public class{
-//new class with code added
+package com.velocity.demo;
+
+public class jump{
+	public static void main(String[] args) {
+		
+	
+System.out.println("Hello Java");
+	}
 }
